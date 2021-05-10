babda427356ff13b55216633886db0a5abc0a01a
gabriel <gabrielfsm@al.insper.edu.br>
2021-05-10 13:50:38 -0300

    atualizacao3

--- principal.py
+++ principal.py
@@ -1,7 +1,13 @@
 from funcoes import *
 
-
-
+print('As 52 cartas de um baralho são embaralhadas e distribuídas em sequência. O objetivo do jogo é colocar todas as cartas em uma mesma pilha.\n')
+print('Existem apenas dois movimentos possíveis:\n')
+print('Empilhar uma carta sobre a carta imediatamente anterior:\n')
+print('Empilhar uma carta sobre a terceira carta anterior.\n')
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:\n')
+print('As duas cartas possuem o mesmo valor ou\n')
+print('As duas cartas possuem o mesmo naipe.\n')
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada')
 #iniciar
 
 continuar = False
@@ -17,7 +23,7 @@ while continuar != True and finalizar == False:
 
 
 #movimentos posiveis 
-while continuar:
+'''while continuar:
     baralho = cria_baralho()
     while possui_movimentos_possiveis(baralho):
         #TODO
@@ -25,8 +31,7 @@ while continuar:
         print('WON')
     else:
         print('you lose')
-
-
+'''
 
 
 
