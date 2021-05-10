f0ec1a41d8f85a75e96a19609ff165e17b6c31be
gabriel <gabrielfsm@al.insper.edu.br>
2021-05-10 13:00:02 -0300

    atualizacao2

--- principal.py
+++ principal.py
@@ -2,16 +2,37 @@ from funcoes import *
 
 
 
-
+#iniciar
 
 continuar = False
 finalizar = False
 while continuar != True and finalizar == False:
-    inicio = input ('START? digite SIM ou NAO') 
+    inicio = input ('START? digite SIM ou NAO\n') 
     if inicio == 'SIM':
         continuar = True
     elif inicio =='NAO':
         finalizar = True
-        print ('GAME OVER')
+        print ('Até logo')
+
+
+
+#movimentos posiveis 
+while continuar:
+    baralho = cria_baralho()
+    while possui_movimentos_possiveis(baralho):
+        #TODO
+    if len(baralho) <= 1:
+        print('WON')
+    else:
+        print('you lose')
+
+
+
+
+
+
+
+
+
 
 
